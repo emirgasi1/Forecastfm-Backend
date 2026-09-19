ca9c08a46fb3eb82948964bdd9bda1d5f3a54da4
Emir <emir.gasi@stu.ibu.edu.ba>
2026-09-19 12:55:33 +0200

    outfit route added

--- src/main/kotlin/routes/OutfitRoutes.kt
+++ src/main/kotlin/routes/OutfitRoutes.kt
@@ -4,12 +4,18 @@ import com.example.outfit.CreateOutfitRequest
 import com.example.outfit.OutfitRepository
 import com.example.outfit.SavedOutfitRepository
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.PartData
+import io.ktor.http.content.forEachPart
+import io.ktor.http.content.streamProvider
 import io.ktor.server.request.receive
+import io.ktor.server.request.receiveMultipart
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
+import java.io.File
+import java.util.UUID
 
 fun Route.outfitRoutes() {
     val outfitRepository = OutfitRepository()
@@ -111,5 +117,34 @@ fun Route.outfitRoutes() {
         } else {
             call.respond(outfit)
         }
+    }
+    post("/api/uploads/image") {
+        val multipart = call.receiveMultipart()
+        var fileName: String? = null
+
+        multipart.forEachPart { part ->
+            if (part is PartData.FileItem) {
+                val originalName = part.originalFileName ?: "image.jpg"
+                val ext = originalName.substringAfterLast('.', "jpg")
+                val generatedName = "${UUID.randomUUID()}.$ext"
+                fileName = generatedName
+
+                val uploadDir = File("uploads")
+                if (!uploadDir.exists()) uploadDir.mkdirs()
+
+                val targetFile = File(uploadDir, generatedName)
+                part.streamProvider().use { input ->
+                    targetFile.outputStream().use { output ->
+                        input.copyTo(output)
+                    }
+                }
+            }
+            part.dispose()
+        }
+
+        val uploaded = fileName
+            ?: return@post call.respond(HttpStatusCode.BadRequest, "No file uploaded")
+
+        call.respond(HttpStatusCode.OK, mapOf("url" to "/uploads/$uploaded"))
     }
 }
